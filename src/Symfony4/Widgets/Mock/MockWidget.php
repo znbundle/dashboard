c0a38a18dbff0c696d8a42fffdde95a103fec90b
Vitaliy <theyamshikov@yandex.ru>
2021-08-03 21:05:13 +0600

    upd

---
+++ src/Symfony4/Widgets/Mock/MockWidget.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ZnBundle\Dashboard\Symfony4\Widgets\Mock;
+
+use ZnLib\Web\Widgets\Base\BaseWidget2;
+
+class MockWidget extends BaseWidget2
+{
+
+    private $html;
+
+    public function getHtml()
+    {
+        return $this->html;
+    }
+
+    public function setHtml($html): void
+    {
+        $this->html = $html;
+    }
+
+    public function run(): string
+    {
+        return $this->html;
+    }
+}
